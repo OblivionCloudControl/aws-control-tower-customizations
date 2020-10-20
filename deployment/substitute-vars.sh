c83e7c3b88af17984b78c672d4cd54d8048607c7
Elvin Luff <elvin@eelviny.me>
2020-10-20 11:13:23 +0200

    Fix substitute vars typo

--- deployment/substitute-vars.sh
+++ deployment/substitute-vars.sh
@@ -3,7 +3,7 @@ for VAR in deployment/custom-control-tower-buckets.cfn.yml deployment/custom-con
 do
     sed -i \
     -e "s/%SOLUTION_NAME%/$SOLUTION_NAME/" \
-    -e "s/%VERSION%/$VERSION`/" \
+    -e "s/%VERSION%/$VERSION/" \
     -e "s/%TEMPLATE_BUCKET_NAME%/$TEMPLATE_BUCKET_NAME/" \
     -e "s/%DIST_BUCKET_NAME%/$DIST_BUCKET_NAME/" \
     $VAR
