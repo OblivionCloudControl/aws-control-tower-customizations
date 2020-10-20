3ad26522fecdf874b7ee5d86dc8d59108ec8aafc
Elvin Luff <elvin@eelviny.me>
2020-10-20 11:02:50 +0200

    Move substitution to environment variables

--- deployment/substitute-vars.sh
+++ deployment/substitute-vars.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 for VAR in deployment/custom-control-tower-buckets.cfn.yml deployment/custom-control-tower-initiation.template
 do
     sed -i \
-    -e "s/%SOLUTION_NAME%/Ripe Control Tower custom/" \
-    -e "s/%VERSION%/`git describe --tags --abbrev=0`/" \
-    -e "s/%TEMPLATE_BUCKET_NAME%/custom-control-tower-templates/" \
-    -e "s/%DIST_BUCKET_NAME%/custom-control-tower-dist/" \
+    -e "s/%SOLUTION_NAME%/$SOLUTION_NAME/" \
+    -e "s/%VERSION%/$VERSION`/" \
+    -e "s/%TEMPLATE_BUCKET_NAME%/$TEMPLATE_BUCKET_NAME/" \
+    -e "s/%DIST_BUCKET_NAME%/$DIST_BUCKET_NAME/" \
     $VAR
 done
